84e487607a95962e899b3d376b701fa9d2495db6
BulaSete <36999228+xkatld@users.noreply.github.com>
2024-07-27 09:51:27 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -13,8 +13,10 @@ $(document).ready(function() {
             },
             success: function(response) {
                 $('#result').html(
-                    '<div class="ip-info"><span class="ip-label">内网 IP：</span>' + response.internal_ip + '</div>' +
-                    '<div class="ip-info"><span class="ip-label">公网 IP：</span>' + response.public_ip + '</div>'
+                    '<div class="ip-info"><span class="ip-label">内网 IPv4：</span>' + response.internal_ipv4 + '</div>' +
+                    '<div class="ip-info"><span class="ip-label">内网 IPv6：</span>' + response.internal_ipv6 + '</div>' +
+                    '<div class="ip-info"><span class="ip-label">公网 IPv4：</span>' + response.public_ipv4 + '</div>' +
+                    '<div class="ip-info"><span class="ip-label">公网 IPv6：</span>' + response.public_ipv6 + '</div>'
                 );
             },
             error: function() {
