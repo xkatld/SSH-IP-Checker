4431e6b2efd7bc9acb3ebe402ea0217fdfa33b68
BulaSete <36999228+xkatld@users.noreply.github.com>
2024-07-27 10:49:29 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#ssh-form').submit(function(e) {
         e.preventDefault();
         $('#result').html('<p>正在查询，请稍候...</p>');
+        $('#error-message').html('');
         $.ajax({
             url: '/',
             method: 'POST',
@@ -12,15 +13,22 @@ $(document).ready(function() {
                 password: $('#password').val()
             },
             success: function(response) {
-                $('#result').html(
-                    '<div class="ip-info"><span class="ip-label">内网 IPv4：</span>' + response.internal_ipv4 + '</div>' +
-                    '<div class="ip-info"><span class="ip-label">内网 IPv6：</span>' + response.internal_ipv6 + '</div>' +
-                    '<div class="ip-info"><span class="ip-label">公网 IPv4：</span>' + response.public_ipv4 + '</div>' +
-                    '<div class="ip-info"><span class="ip-label">公网 IPv6：</span>' + response.public_ipv6 + '</div>'
-                );
+                console.log("Received response:", response);
+                if (response.internal_ipv4 || response.internal_ipv6 || response.public_ipv4 || response.public_ipv6) {
+                    $('#result').html(
+                        '<div class="ip-info"><span class="ip-label">内网 IPv4：</span>' + (response.internal_ipv4 || 'N/A') + '</div>' +
+                        '<div class="ip-info"><span class="ip-label">内网 IPv6：</span>' + (response.internal_ipv6 || 'N/A') + '</div>' +
+                        '<div class="ip-info"><span class="ip-label">公网 IPv4：</span>' + (response.public_ipv4 || 'N/A') + '</div>' +
+                        '<div class="ip-info"><span class="ip-label">公网 IPv6：</span>' + (response.public_ipv6 || 'N/A') + '</div>'
+                    );
+                } else {
+                    $('#result').html('<p>无法获取 IP 地址，请检查输入信息是否正确。</p>');
+                }
             },
-            error: function() {
-                $('#result').html('<p>发生错误，请重试。</p>');
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error("AJAX error:", textStatus, errorThrown);
+                $('#error-message').html('发生错误，请重试。错误信息: ' + textStatus);
+                $('#result').html('');
             }
         });
     });
